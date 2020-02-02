285b0e1377bccc232c44e57270a56e34c54cf79c
Dani Akash <DaniAkash@users.noreply.github.com>
2020-02-02 04:37:11 +0000

    created express server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-var http = require("http");
+const express = require("express");
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello");
+});
+
+const server = app.listen(8080, () => {
+  console.log(`Server running in port ${server.address().port}`)
+})
