9d9d821ded5fee522367ab676e267c6f572f49da
Dani Akash <DaniAkash@users.noreply.github.com>
2020-02-02 06:52:51 +0000

    created student route

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-#ES6 Features used in this project
+# ES6 Features used in this project
 
 - Template Literals/Strings
 - Object Shorthand notation
+- Object Destructuring
+
+## Javascript concepts learnt
+- Truthy & Falsy values
+- Casting types from strings to integers
+
+## Important Express Concepts covered in this project
+
+- Creating routes
+- Creating and using middlewares
+- Body Parser
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const students = require("./models/Students");
 
 const app = express();
+
+app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   // res.send("Response from Middleware");
+//   req.customKey = "Value set in the middleware";
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
 app.get("/students", (req, res) => {
-
   /**
    * Express is smart enough to figure out the
    * response header's MIME type
@@ -24,10 +32,29 @@ app.get("/students", (req, res) => {
   // res.json({students}); // These two statements can be chained together
 
   /**
-   * It's a good practice to be explicit 
+   * It's a good practice to be explicit
    * of the status codes and response types
    */
   res.status(200).json({ students });
+});
+
+app.post("/students", (req, res) => {
+  if (req.body.id && req.body.firstName) {
+    students.push(req.body);
+    res.status(200).json({ message: "Student created successfully" });
+  } else {
+    res.status(400).send("Bad Request");
+  }
+});
+
+app.get("/student/:id", (req, res) => {
+  // const studentId = req.params.id;
+  const { id = "" } = req.params;
+  const requiredStudent = students.find(student => {
+    if (parseInt(id) === student.id) return true;
+    else return false;
+  });
+  res.status(200).json({ student: requiredStudent });
 });
 
 const server = app.listen(8080, () => {
