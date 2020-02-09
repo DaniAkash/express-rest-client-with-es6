21391c29a57b4ddc074bf6081203ef9f938f36f7
Dani Akash <DaniAkash@users.noreply.github.com>
2020-02-09 04:39:31 +0000

    created students router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const students = require("./models/Students");
+const studentsRouter = require("./routers/studentsRouter");
 
 const app = express();
 
@@ -16,36 +17,7 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.get("/students", (req, res) => {
-  /**
-   * Express is smart enough to figure out the
-   * response header's MIME type
-   */
-  // res.send(students);
-  // res.send("<h1>Hello</h1>")
-
-  /**
-   * Multiple properties of the same object of the express modules
-   * can be chained together
-   */
-  // res.status(200);
-  // res.json({students}); // These two statements can be chained together
-
-  /**
-   * It's a good practice to be explicit
-   * of the status codes and response types
-   */
-  res.status(200).json({ students });
-});
-
-app.post("/students", (req, res) => {
-  if (req.body.id && req.body.firstName) {
-    students.push(req.body);
-    res.status(200).json({ message: "Student created successfully" });
-  } else {
-    res.status(400).send("Bad Request");
-  }
-});
+app.use("/students", studentsRouter);
 
 app.get("/student/:id", (req, res) => {
   // const studentId = req.params.id;
---
+++ src/routers/studentsRouter.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const students = require("../models/Students");
+
+const studentsRouter = express.Router();
+
+studentsRouter
+  .get("/", (req, res) => {
+    /**
+     * Express is smart enough to figure out the
+     * response header's MIME type
+     */
+    // res.send(students);
+    // res.send("<h1>Hello</h1>")
+
+    /**
+     * Multiple properties of the same object of the express modules
+     * can be chained together
+     */
+    // res.status(200);
+    // res.json({students}); // These two statements can be chained together
+
+    /**
+     * It's a good practice to be explicit
+     * of the status codes and response types
+     */
+    res.status(200).json({ students });
+  })
+  .post("/", (req, res) => {
+    if (req.body.id && req.body.firstName) {
+      students.push(req.body);
+      res.status(200).json({ message: "Student created successfully" });
+    } else {
+      res.status(400).send("Bad Request");
+    }
+  });
+
+  module.exports = studentsRouter;
