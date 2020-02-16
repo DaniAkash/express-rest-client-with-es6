1e50e1e1ec46663e03f806c3b90b960aded98627
Dani Akash <DaniAkash@users.noreply.github.com>
2020-02-16 05:50:04 +0000

    created hero layout for homepage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
+const path = require("path");
 const studentsRouter = require("./routers/studentsRouter");
 const studentRouter = require("./routers/studentRouter");
 
 const app = express();
 
+/**
+ * Configuring express to use handlebars
+ */
+const hbs = expressHbs.create({
+  extname: ".hbs",
+  layoutsDir: path.join(__dirname, "./views/layouts"),
+  partialsDir: path.join(__dirname, "./views/partials")
+});
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+// including __dirname cuz the following line gets called when code is running as a process
+app.set("views", path.join(__dirname, "./views"));
+
+/**
+ * Middleware for reading json data from
+ * request body
+ */
 app.use(bodyParser.json());
 
 // app.use((req, res, next) => {
@@ -14,7 +33,9 @@ app.use(bodyParser.json());
 // });
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.render("home", {
+    layout: "hero"
+  });
 });
 
 app.use("/students", studentsRouter);
